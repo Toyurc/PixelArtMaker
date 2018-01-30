c4122cb2fe7f52c578760211a598037ac3432281
toyosi.adebayo-ige <toyosi.adebayo-ige@konga.com>
2018-01-30 11:21:49 +0100

    modified design.js

--- designs.js
+++ designs.js
@@ -4,7 +4,28 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    // Your code goes here!
+    var theHeight = $('#inputHeight').val();
+    var theWidth = $('#inputWeight').val();
+    var table = $("#pixelCanvas");
 
-// Your code goes here!
-
+    var tbody = $('<tbody></tbody>');
+    for (var i = 0; i < theHeight; i++) {
+        var tr = $('<tr></tr>');
+        for (var j = 0; j < theWidth; j++) {
+            var td = $('<td id="id"></td>');
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+    table.append(tbody);
 }
+
+function clearTable() {
+    $('tbody').remove();
+}
+//For color picker       
+$(document).on('click', '#id', function () {
+    var color = $('#colorPicker').val();
+    $(this).css('background-color', color);
+});
